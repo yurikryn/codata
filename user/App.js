4e6f070ff4cfa8aa6ec7b46508f612ec7294ac51
yurikryn <yurikryn@gmail.com>
2019-01-04 02:05:26 +0200

    introduced navigation

--- user/App.js
+++ user/App.js
@@ -1,63 +1,16 @@
-import React, {Component} from 'react';
-import {FlatList, ActivityIndicator, Button, StyleSheet, View, Alert } from 'react-native';
+//import React, {Component} from 'react';
+import { createStackNavigator, createAppContainer } from 'react-navigation';
+import SelectorScreen from './SelectorScreen';
+//import ValueScreen from './ValueScreen';
 
-export default class extends Component {
-  constructor(props){
-		super(props);
-		this.state = { isLoading: true }
-	}
+const AppNavigator = createStackNavigator(
+		{
+			Selector: SelectorScreen,
+//			Value: ValueScreen
+		},
+		{
+			initialRouteName: "Selector"
+		}
+);
 
-	componentDidMount(){
-		return (
-			fetch('http://192.168.1.17:8080/api/codata/UNIVERSAL/')
-			.then((response) => response.json())
-			.then((responseJson) => {
-				this.setState({
-					isLoading: false,
-					dataSource: responseJson,
-				});
-			})
-			.catch( (error) => {console.error(error);} )
-		)
-	}	
-
-	
-  render(){
-		if(this.state.isLoading){
-      return(
-        <View style={{flex: 1, padding: 20}}>
-          <ActivityIndicator/>
-        </View>
-      )
-    }
-		return (
-			<View style = { styles.container }>
-			<FlatList
-				data = {this.state.dataSource}
-				renderItem = { ({item,index}) => 
-          <Button 
-            style = { styles.button }
-            onPress = { () => Alert.alert(`${item} = ${index}`)} 
-            title = {item}
-					/>
-				}
-				keyExtractor = {(item, index) => `${index}`}
-			/>
-			</View>
-		);
-	}
-}
-
-const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    padding: 30,
-    backgroundColor: '#fff',
-    alignItems: 'flex-start',
-    justifyContent: 'center',
-  },
-  button:{
-    padding:30,
-    backgroundColor: 'red'
-  }
-}); 
+export default createAppContainer(AppNavigator);
