662e1a08f051012f497fcc29debcd37f417a676d
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-05-27 16:16:26 +0300

    add sorting to codata-object

--- storage/save-codata-object-json.js
+++ storage/save-codata-object-json.js
@@ -1,7 +1,7 @@
-function saveCodataObjectJSON(codataObjectJSON) {
+function saveCodataObjectJSON(codataObjectJSON, fileName) {
 
 	document.getElementById("save-button").onclick = function () {
-		document.getElementById("save-anchor").download = "codata-object.js";
+		document.getElementById("save-anchor").download = fileName;
 		document.getElementById("save-anchor").href = "data:application/javascript;charset=utf-8,"
 			+ encodeURIComponent("let codataObject = " + codataObjectJSON + ";");
 		document.getElementById("save-anchor").click();
