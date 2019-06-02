a6fb4b0ea65590979c701c4aa2e004b5e9fdaecb
yurikryn <yurikryn@gmail.com>
2019-06-03 00:40:03 +0300

    correct reg exp pattern

--- storage/create.js
+++ storage/create.js
@@ -1,7 +1,7 @@
 function create(newCodataList, valueName) {
 
     const output = [];
-    const pattern = /^(?<name>[^*].*?)(?:\x20{2,})(?<value>-?\d[.\d\x20]*?)(?:\x20e(?<exponent>-?\d+))?(?:\x20{2,})(?:\(exact\)|(?<unc_shift>0\.[0\x20]*)(?<uncertainty>[1-9]\d*))(?:\x20e(?<unc_exp>-?\d+))?(?:\x20{2,})(?<unit>.*)$/gm;
+    const pattern = /^(?<name>\S+(?:\x20\S+)*)(?:\x20{2,})(?<value>-?\d{1,4}(?:\x20\d{3})*(?:\.(?:\d{3}\x20)*\d{1,4}(?:\.{3})?)?)(?:\x20e(?<exponent>-?[1-9]\d?))?(?:\x20{2,})(?:\(exact\)|(?<unc_shift>0\.(?:0{3}\x20)*0{0,3})(?<uncertainty>[1-9]\d?))(?:\x20e(?<unc_exp>-?[1-9]\d?))?(?:\x20{2,})(?<unit>[-\d\w^_\x20()/]+)?$/gm;
     let res, i = 0;
     while (res = pattern.exec(newCodataList)) {
         ++i;
@@ -14,12 +14,11 @@ function create(newCodataList, valueName) {
         output.push({
             category: "TODO",
             name,
-            unit: unit || undefined,
+            unit,
             exponent,
-            [`rel-unc`]: unc_shift !== undefined ? `${+(unc_shift.concat(uncertainty).replace(/\x20/g, ``) / value.replace(/\x20/g, ``) * 10 ** 9).toPrecision(2).replace(/^-/, ``)} ppb` : undefined,
+            relative: unc_shift && `${+(unc_shift.concat(uncertainty).replace(/\x20/g, ``) / value.replace(/\x20/g, ``) * 10 ** 9).toPrecision(2).replace(/^-/, ``)} ppb`,
             [valueName]: `${value}${(uncertainty !== undefined) ? `(${uncertainty})` : ``}`
         });
     }
-    // console.log("create");
     return output;
 };
