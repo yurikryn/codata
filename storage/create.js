11f23f181ed40363065f175cfb0159a7f3889d76
Yuri Krynytskyi <ykrynytskyi@clouddevlabs.io>
2019-06-07 20:11:52 +0300

    add fields.length control

--- storage/create.js
+++ storage/create.js
@@ -3,13 +3,14 @@ function create(newCodataList, valueName) {
     const output = [];
 
     const pattern =
-        /^(?:(?<name>\S+(?:\x20\S+)*)(?:\x20{2,})(?<value>-?(?:[1-9]\d{0,2}(?:\x20\d{3})+|[1-9]\d{0,3}|0)(?<valDec>\.(?:\d{3}\x20)*\d{1,4}(?<dots>\.{3})?)?)(?<expField>\x20e(?<exponent>-?[1-9]\d?)|)(?:\x20{2,})(?:\(exact\)|(?<uncertainty>0(?:\.(?:0{3}\x20)*(?:(?=[1-9]\d)|0{1,2}|0{3}(?=[1-9]\x20))|\.)(?<uncertaintyTail>[1-9]\d?))\k<expField>)(?:\x20{2,})(?<unit>[a-zA-Z]+(?:\^(?:[2-9]|-[1-9]))?(?:\x20[a-zA-Z]+(?:\^(?:[2-9]|-[1-9]))?)*|E_h|Hz\x20V_90\^-1|ohm_90|MeV\/c|\(GeV\/c\^2\)\^-2)?|(?<row>.*?))$/gm;
+        /^(?:(?<field1>(?<name>\S+(?:\x20\S+)*)(?:\x20{2,}))(?<field2>(?<value>-?(?:[1-9]\d{0,2}(?:\x20\d{3})+|[1-9]\d{0,3}|0)(?<valDec>\.(?:\d{3}\x20)*\d{1,4}(?<dots>\.{3})?)?)(?<expField>\x20e(?<exponent>-?[1-9]\d?)|)(?:\x20{2,}))(?<field3>(?:\(exact\)|(?<uncertainty>0(?:\.(?:0{3}\x20)*(?:(?=[1-9]\d)|0{1,2}|0{3}(?=[1-9]\x20))|\.)(?<uncertaintyTail>[1-9]\d?))\k<expField>)(?:\x20{2,}))(?<unit>[a-zA-Z]+(?:\^(?:[2-9]|-[1-9]))?(?:\x20[a-zA-Z]+(?:\^(?:[2-9]|-[1-9]))?)*|E_h|Hz\x20V_90\^-1|ohm_90|MeV\/c|\(GeV\/c\^2\)\^-2)?|(?<row>.*?))$/gm;
 
     while (res = pattern.exec(newCodataList)) {
 
-        const { name, value, valDec, dots, exponent, uncertainty, uncertaintyTail, unit, row } = res.groups;
+        const { field1, name, field2, value, valDec, dots, exponent, field3, uncertainty, uncertaintyTail, unit, row } = res.groups;
 
-        if (row || uncertainty && (dots || !valDec || uncertainty.length !== valDec.length + 1)) {
+        if (row || uncertainty && (dots || !valDec || uncertainty.length !== valDec.length + 1)
+            || field1.length !== 60 || field2.length !== 25 || field3.length !== 25) {
             alert(`SOMETHING WRONG IN NEW CONSTANTS !!!\n"${row || name}"`);
             break;
         }
